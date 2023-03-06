af6743257594c91e64ce420a02b8ec5ff40441bb
Lornzyy <lornanish@gmail.com>
2023-03-06 18:02:28 +0300

    thankyou state

---
+++ index.js
@@ -0,0 +1,29 @@
+const containerEl = document.getElementById("container");
+const thankyouState = document.getElementById("thankyou-state");
+
+const submitBtn = document.getElementById("submit-btn");
+const ratingBtn = document.getElementById("rating-btn");
+
+const ratingEl = document.getElementById("rate-given");
+const rates = document.querySelectorAll(".rating-button");
+
+submitBtn.addEventListener("click", function(){
+    return (
+        thankyouState.classList.remove("hidden"),
+        containerEl.style.display = "none"
+    )
+});
+
+ratingBtn.addEventListener("click", function(){
+    return (
+        thankyouState.classList.add("hidden"),
+        containerEl.style.display = "block"
+    )
+});
+
+
+for (let i = 0; i < rates.length; i++){
+    rates[i].addEventListener("click", function(){
+        ratingEl.innerHTML = `You have selected` + rates[i].innerHTML + `out of 5`
+    })
+}
